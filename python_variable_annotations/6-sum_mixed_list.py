560999f42382130a912e149ce14a7daec4c24e27
jrosmanov <cr.osmanov@gmail.com>
2026-02-15 13:05:23 +0400

    task

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""6-sum_mixed_list.py"""
+
+
+import typing
+
+
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
+    '''mixed elements sum'''
+    return sum(mxd_lst)
