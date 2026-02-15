7a7ef9c9bbc9f98d0a2df7cfcdcc7770c08e8318
jrosmanov <cr.osmanov@gmail.com>
2026-02-15 13:08:55 +0400

    task

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""7-to_kv.py"""
+
+
+import typing
+
+
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
+    """kv"""
+    return (k, float(v**2))
+    
