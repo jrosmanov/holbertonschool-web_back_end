8dbb4b33d100952745c78a536e5e181a16994c7e
jrosmanov <cr.osmanov@gmail.com>
2026-02-19 10:52:50 +0400

    task

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """1-concurrent_coroutines.py"""
 import asyncio
-
+from typing import Callable, List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int = 4, max_delay: int = 5) -> List[float]:
     """looping in async"""
