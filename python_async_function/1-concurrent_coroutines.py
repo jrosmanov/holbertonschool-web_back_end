2691dafd6946057c749da9abdb2495fbff7e363d
jrosmanov <cr.osmanov@gmail.com>
2026-02-19 10:51:47 +0400

    task

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""1-concurrent_coroutines.py"""
+import asyncio
+
+
+async def wait_n(n: int = 4, max_delay: int = 5) -> List[float]:
+    """looping in async"""
+    result: List[float] = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(tasks):
+        yielded_result: float = await task
+        result.append(yielded_result)
+
+    return result
