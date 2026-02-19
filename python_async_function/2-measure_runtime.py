cda298795318fa15997725c8a76847a7f68b85c7
jrosmanov <cr.osmanov@gmail.com>
2026-02-19 10:59:45 +0400

    task

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""We are going to check time out"""
+"""function"""
 import time
 import asyncio
 
@@ -7,7 +7,7 @@ wait_n = __import__('2-measure_runtime').wait_n
 
 
 def measure_time(n: int = 4, max_delay: int = 5) -> float:
-    """THis is function baby"""
+    """measuring time"""
     start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.perf_counter()
