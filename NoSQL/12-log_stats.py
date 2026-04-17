aea8fb06cdc1728c08707eecb9bf062fe2be4867
jrosmanov <cr.osmanov@gmail.com>
2026-04-17 14:55:38 +0400

    task

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Provides some stats"""
+import pymongo
+
+
+def log():
+    """Handles printing the stats"""
+    client = pymongo.MongoClient()
+    db = client.logs
+    collection = db.nginx
+
+    print("{} logs".format(collection.count_documents({})))
+
+    print("Methods:")
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+
+    for method in methods:
+        print("method {}: {}".format(
+            method, collection.count_documents({"method": method})))
+
+    print("{} status check".format(collection.count_documents(
+        {"method": "GET", "path": "/status"})))
+
+
+if __name__ == "__main__":
+    log()
