3814c568f200ffa738c862162a4b0c7ba6be5721
jrosmanov <cr.osmanov@gmail.com>
2026-07-05 16:13:13 +0400

    task

---
+++ unittests_in_js/1-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  it("should return 6 when adding 1.4 and 4.5", () => {
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+  });
+
+  it("should return -4 when subtracting 1.4 and 4.5", () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  });
+
+  it("should return 0.2 when dividing 1.4 and 4.5", () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+
+  it("should return 'Error' when dividing 1.4 and 0", () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  });
+});
