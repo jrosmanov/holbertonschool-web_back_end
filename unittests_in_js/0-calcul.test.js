0e9add848778379e1cd9fcea9d8f7a0591ffa8c4
jrosmanov <cr.osmanov@gmail.com>
2026-07-05 16:12:40 +0400

    task

---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it("should return 4 when adding 1 and 3", () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it("should return 5 when adding 1 and 3.7", () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it("should return 5 when adding 1.2 and 3.7", () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it("should return 6 when adding 1.5 and 3.7", () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it("should return 5 when adding 1.7 and 3", () => {
+    assert.strictEqual(calculateNumber(1.7, 3), 5);
+  });
+
+  it("should return -2 when adding -1 and -1", () => {
+    assert.strictEqual(calculateNumber(-1, -1), -2);
+  });
+
+  it("should return -3 when adding -1.2 and -1.7", () => {
+    assert.strictEqual(calculateNumber(-1.2, -1.7), -3);
+  });
+
+  it("should return -3 when adding -1.5 and -1.7", () => {
+    assert.strictEqual(calculateNumber(-1.5, -1.7), -3);
+  });
+});
